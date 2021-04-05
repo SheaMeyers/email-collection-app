63a814431d48ce1237f3fce8f7290dc80b42473f
Shea Meyers <shea.meyers@gmail.com>
2021-04-05 20:07:27 +0200

    Add staff permissions

--- app/admin.py
+++ app/admin.py
@@ -3,10 +3,7 @@ from django.contrib import admin
 from .models import Page, User, EmailEntry
 
 
-@admin.register(Page)
-class PageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'url_pathname', 'title', 'sub_title', 'background_colour',)
-    search_fields = ('url_pathname', 'title', 'sub_title', 'background_colour',)
+admin.site.site_url = None
 
 
 @admin.register(User)
@@ -15,7 +12,37 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ('email',)
 
 
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'url_pathname', 'title', 'sub_title', 'background_colour',)
+    search_fields = ('url_pathname', 'title', 'sub_title', 'background_colour',)
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_active and (request.user.is_staff or request.user.is_superuser)
+
+    def has_module_permission(self, request):
+        return request.user.is_active and (request.user.is_staff or request.user.is_superuser)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        if not request.user.is_superuser:
+            queryset = queryset.filter(user=request.user)
+        return queryset
+
+
 @admin.register(EmailEntry)
 class EmailEntryAdmin(admin.ModelAdmin):
     list_display = ('email', 'date_added',)
     search_fields = ('email',)
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_active and (request.user.is_staff or request.user.is_superuser)
+
+    def has_module_permission(self, request):
+        return request.user.is_active and (request.user.is_staff or request.user.is_superuser)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        if not request.user.is_superuser:
+            queryset = queryset.filter(page__user=request.user)
+        return queryset
