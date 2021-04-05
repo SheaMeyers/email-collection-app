8198094a25100ea77412116f99096cf8d331e291
Shea Meyers <shea.meyers@gmail.com>
2021-04-05 14:31:15 +0200

    Implement email submission

--- app/views.py
+++ app/views.py
@@ -1,10 +1,33 @@
+from django.contrib import messages
 from django.views.generic import DetailView
+from django.views.generic.edit import FormMixin
+from django.urls import reverse
 
-from app.models import Page
+from app.forms import EmailForm
+from app.models import Page, EmailEntry
 
 
-class EmailCollectView(DetailView):
+class EmailCollectView(FormMixin, DetailView):
     model = Page
     slug_field = 'url_pathname'
     slug_url_kwarg = 'url_pathname'
     template_name = 'email_collect.html'
+    form_class = EmailForm
+
+    def get_success_url(self):
+        return reverse('email-collect', kwargs={'url_pathname': self.object.url_pathname})
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            messages.success(request, 'Email successfully submitted!')
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        EmailEntry.objects.create(email=form.cleaned_data['email'], page=self.object)
+        return super(EmailCollectView, self).form_valid(form)
+
+
