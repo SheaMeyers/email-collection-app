e7bd053dcf5779f647de12ac37a6e2de45a3e0c9
Shea Meyers <shea.meyers@gmail.com>
2021-04-24 14:02:00 +0200

    Set django_heroku settings

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,3 +152,5 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "emailcollectwebsite@gmail.com"
 EMAIL_HOST_PASSWORD = "rziuscxsohghrsus"
+
+django_heroku.settings(locals())
